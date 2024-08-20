fe733c99269908ac4ee9d0108f68bbe0c04aaf98
Vadym Chutur <falloutgreck@gmail.com>
2024-08-20 15:57:35 +0300

    upd

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,7 +3,25 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 5050,
+    open: true,
+  },
+  stats: 'errors-only',
 };
